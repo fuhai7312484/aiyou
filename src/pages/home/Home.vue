4d23f43358f4ee7d1274148c6925e0a0a07f88a4
fuhai7312484 <179821617@qq.com>
2018-07-19 22:56:34 +0800

    爱游app

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,12 +1,17 @@
 <template>
 <div>
+    <home-header></home-header>
     <div class="home">这里是首页</div>
     <router-link to="/list">列表页</router-link>
     </div>
 </template>
 <script>
+import HomeHeader from './components/Header'
 export default {
     name:'Home',
+    components: {
+        HomeHeader,
+    }
 
 }
 </script>
